2881f493f520978d9f1850472390f30dd2068c2b
grava <mr.gradilenko@gmail.com>
2017-04-05 14:53:52 +0300

    1

---
+++ src/main/java/com/andersen/model/Car.java
@@ -0,0 +1,29 @@
+package com.andersen.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class Car {
+
+    @Id
+    @GenericGenerator(name = "sequence", strategy = "sequence", parameters = {
+            @org.hibernate.annotations.Parameter(name = "sequenceName", value = "car_sequence"),
+            @org.hibernate.annotations.Parameter(name = "allocationSize", value = "1"),
+    })
+    @GeneratedValue(generator = "sequence", strategy= GenerationType.SEQUENCE)
+    private Long id;
+
+    private String carName;
+
+    private String productionCountry;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JsonIgnore
+    private List<CarModel> models;
+}
---
+++ src/main/java/com/andersen/model/CarModel.java
@@ -0,0 +1,40 @@
+package com.andersen.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NamedStoredProcedureQueries({
+        @NamedStoredProcedureQuery(name = "save_model",
+                procedureName = "cars_project_pkg.save_model",
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "paramModelName", type = String.class),
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "paramYear", type = Integer.class),
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "paramPrice", type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "paramManufactureId", type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.OUT, name = "outParam", type = String.class)
+                })
+})
+public class CarModel {
+
+    @Id
+    @GenericGenerator(name = "sequence", strategy = "sequence", parameters = {
+            @org.hibernate.annotations.Parameter(name = "sequenceName", value = "model_sequence"),
+            @org.hibernate.annotations.Parameter(name = "allocationSize", value = "1"),
+    })
+    @GeneratedValue(generator = "sequence", strategy=GenerationType.SEQUENCE)
+    private Long id;
+
+    private String modelName;
+
+    private Integer manufacturedYear;
+
+    private Long price;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "manufacture_id", referencedColumnName = "id")
+    private Car manufacturerName;
+}
