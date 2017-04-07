ba754cea51c613d5404303a85f8bb8df0d605b74
vgradilenko <mr.gradilenko@gmail.com>
2017-04-07 15:44:10 +0300

    add spring security with in_memory users

---
+++ src/main/java/com/andersen/domain/User.java
@@ -0,0 +1,23 @@
+package com.andersen.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+    private List<Role> authorities;
+    private String password;
+    private String username;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+}
